035e3212cc0864a2b7df83dfd13bcb7d73895f1b
Oleksii Olefirenko <OOlefirenko@wsgc.com>
2022-06-05 21:41:35 +0300

    Initial Commit

---
+++ JenkinsStudy/JenkinsStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  JenkinsStudyApp.swift
+//  JenkinsStudy
+//
+//  Created by Oleksii Olefirenko on 05.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct JenkinsStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
